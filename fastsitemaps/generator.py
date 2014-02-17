10e6407da4233b18b1febaf88f81d123b1ba88b5
Javier Lasheras <jlasheras@qdqmedia.com>
2014-02-17 13:03:49 +0100

    Add extensions features

--- fastsitemaps/generator.py
+++ fastsitemaps/generator.py
@@ -10,7 +10,11 @@ def sitemap_generator(request, maps, page, current_site):
     output = StringIO()
     xml = SimplerXMLGenerator(output, settings.DEFAULT_CHARSET)
     xml.startDocument()
-    xml.startElement('urlset', {'xmlns':'http://www.sitemaps.org/schemas/sitemap/0.9'})
+    namespaces = {'xmlns':'http://www.sitemaps.org/schemas/sitemap/0.9'}
+    for site in maps:
+        ns_getter = getattr(site, "get_namespaces", lambda: {})
+        namespaces.update(ns_getter())
+    xml.startElement('urlset', namespaces)
     yield output.getvalue()
     pos = output.tell()
     for site in maps:
@@ -39,6 +43,35 @@ def sitemap_generator(request, maps, page, current_site):
                     xml.addQuickElement('priority', url['priority'])
             except KeyError:
                 pass
+
+            # Translations
+            translations = url.get("translations", None)
+            if translations:
+                for lang, uri in translations.items():
+                    xml.addQuickElement("xhtml:link", attrs={
+                        "rel": "alternate",
+                        "hreflang": lang,
+                        "href": uri
+                    })
+
+            # Video extension
+            videos = url.get("videos", [])
+            if videos:
+                for video in videos:
+                    xml.startElement("video:video", {})
+                    for tag, value in video.items():
+                        xml.addQuickElement("video:%s" % tag, value)
+                    xml.endElement("video:video")
+
+            # Image extension
+            images = url.get("images", [])
+            if images:
+                for image in images:
+                    xml.startElement("image:image", {})
+                    for tag, value in image.items():
+                        xml.addQuickElement("image:%s" % tag, value)
+                    xml.endElement("image:image")
+
             xml.endElement('url')
             output.seek(pos)
             yield output.read()
--- fastsitemaps/sitemaps.py
+++ fastsitemaps/sitemaps.py
@@ -15,6 +15,22 @@ class RequestSitemap(Sitemap):
             return attr(obj)
         return attr
 
+    _get = __get
+
+    def _get_urlinfo(self, item, site):
+        loc = self.__get('location', item)
+        if not loc.startswith('http'):
+            loc = "http://%s%s" % (site.domain, loc)
+        priority = self.__get('priority', item, None)
+        url_info = {
+            'item':       item,
+            'location':   loc,
+            'lastmod':    self.__get('lastmod', item, None),
+            'changefreq': self.__get('changefreq', item, None),
+            'priority':   str(priority is not None and priority or ''),
+        }
+        return url_info
+
     def get_urls(self, page=1, site=None):
         "Returns a generator instead of a list, also prevents http: doubling up"
         if site is None:
@@ -26,15 +42,52 @@ class RequestSitemap(Sitemap):
             if site is None:
                 raise ImproperlyConfigured("In order to use Sitemaps you must either use the sites framework or pass in a Site or RequestSite object in your view code.")
         for item in self.paginator.page(page).object_list:
-            loc = self.__get('location', item)
-            if not loc.startswith('http'):
-                loc = "http://%s%s" % (site.domain, loc)
-            priority = self.__get('priority', item, None)
-            url_info = {
-                'item':       item,
-                'location':   loc,
-                'lastmod':    self.__get('lastmod', item, None),
-                'changefreq': self.__get('changefreq', item, None),
-                'priority':   str(priority is not None and priority or ''),
-            }
-            yield url_info
+            yield self._get_urlinfo(item, site)
+
+    @classmethod
+    def get_namespaces(self):
+        return {}
+
+
+class VideoSitemapMixin(object):
+    def _get_urlinfo(self, item, site):
+        url_info = super(VideoSitemapMixin, self)._get_urlinfo(item, site)
+        url_info["videos"] = self._get("videos", item, None)
+        return url_info
+
+    @classmethod
+    def get_namespaces(self):
+        namespaces = super(VideoSitemapMixin, self).get_namespaces()
+        namespaces["xmlns:video"] = "http://www.google.com/schemas/sitemap-video/1.1"
+        return namespaces
+
+
+class ImageSitemapMixin(object):
+    def _get_urlinfo(self, item, site):
+        url_info = super(ImageSitemapMixin, self)._get_urlinfo(item, site)
+        url_info["images"] = self._get("images", item, None)
+        return url_info
+
+    @classmethod
+    def get_namespaces(self):
+        namespaces = super(ImageSitemapMixin, self).get_namespaces()
+        namespaces["xmlns:image"] = "http://www.google.com/schemas/sitemap-image/1.1"
+        return namespaces
+
+
+class TranslationSitemapMixin(object):
+    """
+    Your sitemap objects will execute translations function or attribute.
+
+    translations must return a dict with languages as keys and url as value.
+    """
+    def _get_urlinfo(self, item, site):
+        url_info = super(TranslationSitemapMixin, self)._get_urlinfo(item, site)
+        url_info["translations"] = self._get("translations", item, None)
+        return url_info
+
+    @classmethod
+    def get_namespaces(self):
+        namespaces = super(TranslationSitemapMixin, self).get_namespaces()
+        namespaces["xmlns:xhtml"]="http://www.w3.org/1999/xhtml"
+        return namespaces
